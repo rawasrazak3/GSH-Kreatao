2ba1a0922f076ffd83d702d17b03b0ef23ce99a5
rawasrazak3 <rawasrazak3@gmail.com>
2024-08-08 10:29:05 +0530

    OT calc issue2

--- gsh/gsh_kreatao/custom_script/attendance.py
+++ gsh/gsh_kreatao/custom_script/attendance.py
@@ -9,16 +9,13 @@ def calculate_hours(doc, method):
         total_hours = doc.custom_total_hours
 
         difference = working_hours - total_hours
-        print("difference =", difference)
 
         if difference >= 0:
             frappe.db.set_value("Attendance", doc.name, 'over_time_hours', float(difference))
             frappe.db.set_value("Attendance", doc.name, 'late_entry_hours', 0.00)
-            print("overtime=",doc.over_time_hours)
         else:
             frappe.db.set_value("Attendance", doc.name, 'late_entry_hours', float(-difference))
             frappe.db.set_value("Attendance", doc.name, 'over_time_hours', 0.00)
-            print("late entry =",doc.late_entry_hours)
 
 
 def create_compensatory_leave(doc, method):
