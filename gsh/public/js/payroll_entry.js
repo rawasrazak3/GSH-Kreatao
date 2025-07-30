cbc1f127ee6ef79c752fff9718169ce83fbc3a63
NihalaAbdulNasarNK <nihalaabdulnasarnk05@gmail.com>
2025-07-30 18:16:09 +0530

    salary component change

--- gsh/public/js/payroll_entry.js
+++ gsh/public/js/payroll_entry.js
@@ -220,17 +220,17 @@ function calculate_saturday_allowance_deduction(slip) {
                         let deductions = salary_slip.deductions || [];
                         let total_working_days = salary_slip.total_working_days;
 
-                        let overtime_allowance = earnings.find(e => e.salary_component === 'Saturday Fixed Overtime Allowance');
+                        let overtime_allowance = earnings.find(e => e.salary_component === 'Overtime');
                         if (overtime_allowance) {
                             let per_day_amount = overtime_allowance.default_amount / total_working_days;
                             let deduction_amount = per_day_amount * leave_days;
 
-                            let deduction_entry = deductions.find(d => d.salary_component === 'Saturday Allowance Deduction');
+                            let deduction_entry = deductions.find(d => d.salary_component === 'Overtime Allowance Deduction');
                             if (deduction_entry) {
                                 deduction_entry.amount = deduction_amount;
                             } else {
                                 deductions.push({
-                                    salary_component: 'Saturday Allowance Deduction',
+                                    salary_component: 'Overtime Allowance Deduction',
                                     amount: deduction_amount
                                 });
                             }
